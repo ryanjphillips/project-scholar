647172ed8679dfcee26843fce07c0979bbceb2ab
Ryan Phillips <ryan.phillips.j@gmail.com>
2023-03-08 00:54:56 -0600

    REFACTOR: Started the process of refactoring the /ui/chessboard file into a headerfile, functions, and constants.

---
+++ src/ui/chessboard/chessboard.h
@@ -0,0 +1,20 @@
+
+// Link to more information on the following unicode symbols:
+// https://unicodeplus.com/U+2654
+// Declaring and Initializing black chess piece unicode, UTF-8 encoding.
+
+const char blackPawn[] = "\xE2\x99\x9F";
+const char blackRook[] = "\xE2\x99\x9C";
+const char blackBishop[] = "\xE2\x99\x9D";
+const char blackKnight[] = "\xE2\x99\x9E";
+const char blackQueen[] = "\xE2\x99\x9B";
+const char blackKing[] = "\xE2\x99\x9A";
+
+// Declaring and Initializing white chess piece unicode, UTF-8 encoding.
+
+const char whitePawn[] = "\xE2\x99\x99";
+const char whiteRook[] = "\xE2\x99\x96";
+const char whiteBishop[] = "\xE2\x99\x97";
+const char whiteKnight[] = "\xE2\x99\x98";
+const char whiteQueen[] = "\xE2\x99\x95";
+const char whiteKing[] = "\xE2\x99\x94";
