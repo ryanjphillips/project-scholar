a0e5434c2e46e3c5d913ebed830012c6366908df
Ryan Phillips <ryan.phillips.j@gmail.com>
2023-04-04 20:47:19 -0500

    UPDATE: Finished King Movement. To Do: put the CheckOffBoard Function into the LegalMovesFunctions.

--- src/engine/pieces/movement/kingMovement/kingMovement.c
+++ src/engine/pieces/movement/kingMovement/kingMovement.c
@@ -1,2 +1,46 @@
-void kingMovement(struct Tile *pTile, struct Piece *pQueen) {
+void kingMovement(struct Tile *pTile, struct Piece *pKing) {
+	
+	int kingPosition;
+
+	kingPosition = pKing->position;
+
+	if (checkIfNewPositionIsOffBoard(kingPosition  + 8, kingPosition + 8) == 0) {
+
+		checkForLegalMoves(pTile, kingPosition + 8);
+	}
+
+	if (checkIfNewPositionIsOffBoard(kingPosition  - 8, kingPosition - 8) == 0) {
+
+		checkForLegalMoves(pTile, kingPosition - 8);
+	}
+
+	if (checkIfNewPositionIsOffBoard(kingPosition  + 8 + 1, kingPosition + 8 + 1) == 0) {
+
+		checkForLegalMoves(pTile, kingPosition + 8 + 1);
+	}
+
+	if (checkIfNewPositionIsOffBoard(kingPosition  + 8 - 1, kingPosition + 8 - 1) == 0) {
+
+		checkForLegalMoves(pTile, kingPosition + 8 - 1);
+	}
+
+	if (checkIfNewPositionIsOffBoard(kingPosition  + 1, kingPosition + 1) == 0) {
+
+		checkForLegalMoves(pTile, kingPosition + 1);
+	}
+
+	if (checkIfNewPositionIsOffBoard(kingPosition  - 1, kingPosition - 1) == 0) {
+
+		checkForLegalMoves(pTile, kingPosition - 1);
+	}
+
+	if (checkIfNewPositionIsOffBoard(kingPosition  - 8 - 1, kingPosition - 8 - 1) == 0) {
+
+		checkForLegalMoves(pTile, kingPosition - 8 - 1);
+	}
+
+	if (checkIfNewPositionIsOffBoard(kingPosition  - 8 + 1, kingPosition - 8 + 1) == 0) {
+
+		checkForLegalMoves(pTile, kingPosition - 8 + 1);
+	}
 }
